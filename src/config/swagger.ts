6b5a65343eefbe9c4bf81b26c6d4d803c42805ef
Halgand Ervin <halgand.ervin@gmail.com>
2025-02-07 17:00:37 +0100

    📝 initi swagger documentation && .env

---
+++ src/config/swagger.ts
@@ -0,0 +1,32 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+interface swaggerConfig {
+  app: INestApplication<any>;
+  title: string;
+  description?: string;
+  endpoint?: string;
+  version?: string;
+}
+
+const initSwagger = ({
+  app,
+  title,
+  description,
+  endpoint,
+  version,
+}: swaggerConfig) => {
+  const SWAGGER_ROUTE = endpoint ?? 'api';
+
+  const config = new DocumentBuilder()
+    .setTitle(title)
+    .setDescription(description ?? '')
+    .setVersion(version ?? '')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup(SWAGGER_ROUTE, app, documentFactory);
+};
+
+export default initSwagger;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import initSwagger from './config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  initSwagger({
+    app,
+    title: 'Trello',
+    description: 'API documentation for a minified Trello',
+    version: '1.0',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
