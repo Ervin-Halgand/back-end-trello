586d0b5f871845896776c96d34a5c69761042501
Halgand Ervin <halgand.ervin@gmail.com>
2025-02-09 23:15:36 +0100

    📝 document User api with swagger

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,12 +1,22 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({
+    example: 'Tony',
+    description: 'name or pseudo',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   readonly name?: string;
 
+  @ApiProperty({
+    example: 'Fadell',
+    description: 'User lastname',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   readonly lastName?: string;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,22 +12,29 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a user' })
   create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Retrieve a user' })
+  @ApiParam({ name: 'id', type: Number, description: 'User ID' })
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a user' })
+  @ApiParam({ name: 'id', type: Number, description: 'User ID' })
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body(ValidationPipe) updateUserDto: UpdateUserDto,
@@ -36,6 +43,8 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a user' })
+  @ApiParam({ name: 'id', type: Number, description: 'User ID' })
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.remove(id);
   }
