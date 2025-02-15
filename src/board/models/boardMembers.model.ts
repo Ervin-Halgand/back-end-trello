983947400e7dc8ce9084d7377b1badbcaba8149a
Halgand Ervin <halgand.ervin@gmail.com>
2025-02-15 19:53:09 +0100

    ✨  add PK to boardMembers

--- src/board/models/boardMembers.model.ts
+++ src/board/models/boardMembers.model.ts
@@ -1,9 +1,22 @@
-import { Column, ForeignKey, Table, Model } from 'sequelize-typescript';
+import {
+  Column,
+  ForeignKey,
+  Table,
+  Model,
+  DataType,
+} from 'sequelize-typescript';
 import { User } from '../../users/models/user.model';
 import { Board } from './board.model';
 
 @Table
 export class BoardMembers extends Model<BoardMembers> {
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id: number;
+
   @ForeignKey(() => Board)
   @Column
   boardId: number;
