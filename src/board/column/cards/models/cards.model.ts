3399218efdb4c7070d20589ff942ac0bac7f34f8
Halgand Ervin <halgand.ervin@gmail.com>
2025-02-19 14:42:38 +0100

    🗃️ add create table cards migration and model

---
+++ src/board/column/cards/models/cards.model.ts
@@ -0,0 +1,54 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  ForeignKey,
+  BelongsTo,
+} from 'sequelize-typescript';
+import { Column as ColumnModel } from '../../models/Column.model';
+import { User } from 'src/users/models/user.model';
+
+@Table
+export class Cards extends Model<Cards> {
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    unique: true,
+    allowNull: false,
+  })
+  id: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  name: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  description: string;
+
+  @ForeignKey(() => ColumnModel)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  columnId: ColumnModel;
+
+  @BelongsTo(() => ColumnModel)
+  column: ColumnModel;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  @ForeignKey(() => User)
+  assignedUserId: User;
+
+  @BelongsTo(() => User)
+  user: User;
+}
